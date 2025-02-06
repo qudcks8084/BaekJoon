2b007d8eb93761262718ec41cc86f1e2aaed9e5b
Park Byeongchan <47100335+qudcks8084@users.noreply.github.com>
2025-02-06 10:52:07 +0900

    [Silver V] Title: 집합, Time: 980 ms, Memory: 313664 KB -BaekjoonHub

--- 백준/Silver/11723. 집합/집합.java
+++ 백준/Silver/11723. 집합/집합.java
@@ -1,52 +1,42 @@
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
-import java.util.Arrays;
 import java.util.StringTokenizer;
 
 public class Main {
-
-	public static void main(String[] args) throws Exception {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		boolean[] set = new boolean[21];
-		
-		int N = Integer.parseInt(br.readLine());
-		StringTokenizer st;
-		StringBuilder sb = new StringBuilder();
-		for(int i = 0 ; i < N ; i++) {
-			st = new StringTokenizer(br.readLine());
-			String operator = st.nextToken();
-			if(operator.equals("add")) {
-				int target = Integer.parseInt(st.nextToken());
-				set[target] = true;
-				continue;
-			}
-			if(operator.equals("check")) {
-				int target = Integer.parseInt(st.nextToken());
-				if(set[target]) sb.append("1\n");
-				else sb.append("0\n");
-				
-				continue;
-			}
-			if(operator.equals("remove")) {
-				int target = Integer.parseInt(st.nextToken());
-				set[target] = false;
-				continue;
-			}
-			if(operator.equals("toggle")) {
-				int target = Integer.parseInt(st.nextToken());
-				set[target] = !set[target];
-				continue;
-			}
-			if(operator.equals("all")) {
-				Arrays.fill(set, true);
-				continue;
-			}
-			if(operator.equals("empty")) {
-				Arrays.fill(set, false);
-				continue;
-			}
-		}
-		System.out.println(sb);
-	}
-
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int bitSet = 0;
+        int N = Integer.parseInt(br.readLine());
+        StringBuilder sb = new StringBuilder();
+        
+        for (int i = 0; i < N; i++) {
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            String operator = st.nextToken();
+            
+            if (operator.equals("add")) {
+                int target = Integer.parseInt(st.nextToken());
+                bitSet |= (1 << target);
+            } 
+            else if (operator.equals("remove")) {
+                int target = Integer.parseInt(st.nextToken());
+                bitSet &= ~(1 << target);
+            } 
+            else if (operator.equals("check")) {
+                int target = Integer.parseInt(st.nextToken());
+                sb.append((bitSet & (1 << target)) != 0 ? "1\n" : "0\n");
+            } 
+            else if (operator.equals("toggle")) {
+                int target = Integer.parseInt(st.nextToken());
+                bitSet ^= (1 << target);
+            } 
+            else if (operator.equals("all")) {
+                bitSet = (1 << 21) - 1;
+            } 
+            else if (operator.equals("empty")) {
+                bitSet = 0;
+            }
+        }
+        
+        System.out.println(sb);
+    }
 }
