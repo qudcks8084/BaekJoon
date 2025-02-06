f0d9656285664668179bf9b1e1e7e522545b30aa
Park Byeongchan <47100335+qudcks8084@users.noreply.github.com>
2025-02-06 17:23:03 +0900

    [Silver III] Title: 소수 구하기, Time: 580 ms, Memory: 20736 KB -BaekjoonHub

--- 백준/Silver/1929. 소수 구하기/소수 구하기.java
+++ 백준/Silver/1929. 소수 구하기/소수 구하기.java
@@ -1,31 +1,23 @@
-import java.util.Arrays;
+
 import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int N = sc.nextInt();
-
         if(N == 1) N++;
         int M = sc.nextInt();
-        int len = M - N + 1;
-
-        int[] arr = new int[len];
-
-        for(int i = 0 ; i < len ; i++){
-            arr[i] = N + i;
-        }
-        
-        for(int i = 2 ; i <= Math.sqrt(M) ; i++){
-            for(int j = 0 ; j < len ; j++){
-                if(arr[j] == 0) continue;
-                if(arr[j] != i && arr[j] % i == 0) arr[j] = 0;
-            }
-        }
 
         StringBuilder sb = new StringBuilder();
-        for(int i : arr){
-            if(i != 0) sb.append(i).append("\n");
+        for(int i = N ; i <= M ; i++) {
+        	boolean flag = true;
+        	for(int j = 2 ; j <= Math.sqrt(M) ; j++) {
+        		if(i != j && i % j == 0) {
+        			flag = false;
+        			break;
+        		} 
+        	}
+        	if(flag) sb.append(i).append("\n");
         }
         System.out.println(sb);
     }
