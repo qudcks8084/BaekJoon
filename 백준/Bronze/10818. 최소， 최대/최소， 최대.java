3600490a5ece9fe40e15d1ee534ff2d43b3bab3d
Park Byeongchan <47100335+qudcks8084@users.noreply.github.com>
2025-02-07 22:55:50 +0900

    [Bronze III] Title: 최소, 최대, Time: 1240 ms, Memory: 135916 KB -BaekjoonHub

--- 백준/Bronze/10818. 최소， 최대/최소， 최대.java
+++ 백준/Bronze/10818. 최소， 최대/최소， 최대.java
@@ -1,19 +1,24 @@
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+import java.util.TreeSet;
 
-public class Main{
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
+public class Main {
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
-        int n = sc.nextInt();
+        TreeSet<Integer> set = new TreeSet<>();
 
-        
-        int max = Integer.MIN_VALUE;
-        int min = Integer.MAX_VALUE;
-        for(int i = 0 ; i < n; i++){
-            int b = sc.nextInt();
-            max = Math.max(b, max);
-            min = Math.min(b, min);
+        int N = Integer.parseInt(br.readLine());
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        for(int i = 0 ; i < N ; i ++){
+            set.add(Integer.parseInt(st.nextToken()));
         }
-        System.out.println(min + " " + max);
+
+        StringBuilder sb = new StringBuilder();
+        sb.append(set.first()).append(" ").append(set.last());
+        System.out.println(sb);
+
+
     }
 }
