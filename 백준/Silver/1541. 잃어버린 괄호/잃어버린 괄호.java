fb7533386b7aa6bf061bb2f36b36d36c465eb247
Park Byeongchan <47100335+qudcks8084@users.noreply.github.com>
2025-02-08 00:00:54 +0900

    [Silver II] Title: 잃어버린 괄호, Time: 168 ms, Memory: 17592 KB -BaekjoonHub

--- 백준/Silver/1541. 잃어버린 괄호/잃어버린 괄호.java
+++ 백준/Silver/1541. 잃어버린 괄호/잃어버린 괄호.java
@@ -1,29 +1,37 @@
-import java.util.ArrayDeque;
 import java.util.Scanner;
 import java.util.StringTokenizer;
 
 public class Main {
 
-    static ArrayDeque<String> calculator;
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         String input = sc.next();
-        String[] nums = input.split("-");
+        int minus_index = input.indexOf("-");
 
-        int sum = 0;
-        StringTokenizer plus = new StringTokenizer(nums[0], "+");
-        while (plus.hasMoreTokens()) {
-            sum += Integer.parseInt(plus.nextToken());
-        }
-
-        for(int i = 1 ; i < nums.length ; i++){
-            StringTokenizer minus = new StringTokenizer(nums[i], "+");
-            while(minus.hasMoreTokens()){
-                sum -= Integer.parseInt(minus.nextToken());
+        if (minus_index == -1) { // 더하기 밖에 없다.
+            int sum = 0;
+            StringTokenizer st = new StringTokenizer(input, "+");
+            while (st.hasMoreTokens()) {
+                sum += Integer.parseInt(st.nextToken());
             }
+            System.out.println(sum);
         }
+        else{ // 빼기 연산이 존재한다.
+            String plus = input.substring(0, minus_index);
+            String minus = input.substring(minus_index);
 
-        System.out.println(sum);
+            int sum = 0;
+            StringTokenizer st = new StringTokenizer(plus, "+");
+            while (st.hasMoreTokens()) {
+                sum += Integer.parseInt(st.nextToken());
+            }
+
+            st = new StringTokenizer(minus, "+-");
+            while(st.hasMoreTokens()){
+                sum -= Integer.parseInt(st.nextToken());
+            }
+
+            System.out.println(sum);
+        }
     }
-
 }
