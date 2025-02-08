b6ae02c6852ac4dbf40813cd52595b536cfdc9b0
Park Byeongchan <47100335+qudcks8084@users.noreply.github.com>
2025-02-08 14:56:47 +0900

    [Bronze III] Title: 최소, 최대, Time: 424 ms, Memory: 89640 KB -BaekjoonHub

--- 백준/Bronze/10818. 최소， 최대/최소， 최대.java
+++ 백준/Bronze/10818. 최소， 최대/최소， 최대.java
@@ -1,30 +1,28 @@
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
-import java.util.ArrayList;
-import java.util.Collections;
 import java.util.StringTokenizer;
 
 public class Main {
     public static void main(String[] args) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
-        int[] arr = new int[2000001];
+        boolean[] arr = new boolean[2000001];
 
         int N = Integer.parseInt(br.readLine());
         StringTokenizer st = new StringTokenizer(br.readLine());
         for(int i = 0 ; i < N ; i ++){
-            arr[Integer.parseInt(st.nextToken())+1000000]++;
+            arr[Integer.parseInt(st.nextToken())+1000000] = true;
         }
 
         StringBuilder sb = new StringBuilder();
         for(int i = 0 ; i < 2000000 ; i++){
-            if(arr[i] != 0){
+            if(arr[i]){
                 sb.append(i-1000000).append(" ");
                 break;
             }
         }
         for(int i = 2000000 ; i >= 0 ; i--){
-            if(arr[i] != 0){
+            if(arr[i]){
                 sb.append(i-1000000);
                 break;
             }
