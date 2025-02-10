831fae4b9919ca3db135e4922fdf2b3c68a60d92
Park Byeongchan <47100335+qudcks8084@users.noreply.github.com>
2025-02-10 12:57:43 +0900

    [Gold V] Title: 배열 돌리기 1, Time: 464 ms, Memory: 34468 KB -BaekjoonHub

---
+++ 백준/Gold/16926. 배열 돌리기 1/배열 돌리기 1.java
@@ -0,0 +1,92 @@
+
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.ArrayDeque;
+import java.util.StringTokenizer;
+
+public class Main {
+	
+	static int H, W, R;
+	static int[][] map;
+	static boolean[][] visited;
+	public static void main(String[] args) throws Exception {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		H = Integer.parseInt(st.nextToken());
+		W = Integer.parseInt(st.nextToken());
+		R = Integer.parseInt(st.nextToken());
+		
+		map = new int[H][W];
+		visited = new boolean[H][W];
+		
+		for(int c = 0 ; c < H ; c++) {
+			st = new StringTokenizer(br.readLine());
+			for(int r = 0 ; r < W ; r++) {
+				map[c][r] = Integer.parseInt(st.nextToken());
+			}
+		}
+		
+		int num = 0;
+		while(true) {
+			if(visited[num][num]) break;
+			rotate(num, num, num+1);
+			num++;
+		}
+		print();
+		
+	}
+	
+	public static void rotate(int c, int r, int k) {
+		
+		// 순서를 회전할 각각의 데이터를 수집
+		ArrayDeque<Integer> q = new ArrayDeque<>();
+		while(c < H-k) {
+			q.offer(map[c++][r]);
+		}
+		while(r < W-k) {
+			q.offer(map[c][r++]);
+		}
+		while(c >= k) {
+			q.offer(map[c--][r]);
+		}
+		while(r >= k) {
+			q.offer(map[c][r--]);
+		}
+		
+		// 배열 돌리기
+		for(int i = 0 ; i < R ; i++) {
+			q.addFirst(q.removeLast());
+		}
+		
+		// 다시 배열 저장하기 
+		while(c < H-k) {
+			visited[c][r] = true;
+			map[c++][r] = q.poll();
+		}
+		while(r < W-k) {
+			visited[c][r] = true;
+			map[c][r++] = q.poll();
+		}
+		while(c >= k) {
+			visited[c][r] = true;
+			map[c--][r] = q.poll();
+		}
+		while(r >= k) {
+			visited[c][r] = true;
+			map[c][r--] = q.poll();
+		}
+	}
+	
+	public static void print() {
+		StringBuilder sb = new StringBuilder();
+		for(int c = 0 ; c < H ; c++) {
+			for(int r = 0 ; r < W ; r++) {
+				sb.append(map[c][r]).append(" ");
+			}
+			sb.append("\n");
+		}
+		System.out.println(sb);
+	}
+}
