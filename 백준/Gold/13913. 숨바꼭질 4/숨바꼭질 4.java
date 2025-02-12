6846645dadc8880cba0765adba2eb521910b3e1e
Park Byeongchan <47100335+qudcks8084@users.noreply.github.com>
2025-02-12 11:31:44 +0900

    [Gold IV] Title: 숨바꼭질 4, Time: 248 ms, Memory: 33804 KB -BaekjoonHub

--- 백준/Gold/13913. 숨바꼭질 4/숨바꼭질 4.java
+++ 백준/Gold/13913. 숨바꼭질 4/숨바꼭질 4.java
@@ -9,58 +9,62 @@ public class Main {
     static int N, K;
     static boolean[] visited;
     static int[] path;
-    static ArrayDeque<Integer> stack;
+    static StringBuilder sb;
 
     public static void main(String[] args) throws Exception {
 
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        stack = new ArrayDeque<>();
         StringTokenizer st = new StringTokenizer(br.readLine());
+        sb = new StringBuilder();
+        
         N = Integer.parseInt(st.nextToken());
         K = Integer.parseInt(st.nextToken());
         visited = new boolean[100001];
         path = new int[100001];
 
-        StringBuilder sb = new StringBuilder();
+        
         if(N == K){
             sb.append(0).append("\n").append(N);
         }else{
             find();
-            sb.append(stack.size()-1).append("\n");
-            while(!stack.isEmpty()){
-                sb.append(stack.pop()).append(" ");
-            }
         }
 
         System.out.println(sb);
+ 
     }
 
     public static void find(){
         Queue<Integer> q = new ArrayDeque<>();
         q.offer(N);
         visited[N] = true;
+        int time = 0;
         while (!q.isEmpty()) {
-            int now = q.poll();
-            int[] next_position = new int[]{now - 1, now + 1, now * 2};
-            for (int next : next_position) {
-                if(next < 0 || next > 100000) continue;
-                if(next == K){
-                    path[next] = now;
-                    print(next);
-                    return;
+        	int len = q.size();
+        	for(int i = 0 ; i < len ; i++) {
+        		int now = q.poll();
+                int[] next_position = new int[]{now - 1, now + 1, now * 2};
+                for (int next : next_position) {
+                    if(next < 0 || next > 100000) continue;
+                    if(next == K){
+                    	sb.append(time+1).append("\n");
+                        path[next] = now;
+                        print(next);
+                        return;
+                    }
+                    if(!visited[next]){
+                        path[next] = now;
+                        visited[next] = true;
+                        q.offer(next);
+                    }
                 }
-                if(!visited[next]){
-                    path[next] = now;
-                    visited[next] = true;
-                    q.offer(next);
-                }
-            }
+        	}
+        	time++;
         }
     }
 
     public static void print(int before){
-        stack.push(before);
         if(before != N)
             print(path[before]);
+        sb.append(before).append(" ");
     }
 }
