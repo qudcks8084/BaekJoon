cbcac9ca69108b81ceaf706effa8cdfd45762edd
Park Byeongchan <47100335+qudcks8084@users.noreply.github.com>
2025-03-23 11:46:52 +0900

    [Silver V] Title: 드론 조작, Time: 132 ms, Memory: 109804 KB -BaekjoonHub

---
+++ 백준/Silver/32932. 드론 조작/드론 조작.java
@@ -0,0 +1,65 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+
+    static int[] dc = {-1, 0, 1, 0};
+    static int[] dr = {0, 1, 0, -1};
+
+    static boolean[][] map;
+    static int M = 10000;
+
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int N = Integer.parseInt(st.nextToken());
+        int R = Integer.parseInt(st.nextToken());
+
+        map = new boolean[M][M];
+
+        for(int i = 0 ; i < N ; i++){
+            st = new StringTokenizer(br.readLine());
+            int x = Integer.parseInt(st.nextToken()) + M / 2;
+            int y = Integer.parseInt(st.nextToken()) + M / 2;
+            map[x][y] = true;
+        }
+
+        int sx = 5000;
+        int sy = 5000;
+
+        char[] command = br.readLine().toCharArray();
+
+        for(char c : command){
+            if(c == 'U'){
+                int n_y = sy + 1;
+                if(n_y >= M || map[sx][n_y]) continue;
+                sy = n_y;
+            }
+            else if(c == 'D'){
+                int n_y = sy - 1;
+                if(n_y < 0 || map[sx][n_y]) continue;
+                sy = n_y;
+            }
+            else if(c == 'R'){
+                int n_x = sx + 1;
+                if(n_x >= M || map[n_x][sy]) continue;
+                sx = n_x;
+            }
+            else {
+                int n_x = sx - 1;
+                if(n_x < 0 || map[n_x][sy]) continue;
+                sx = n_x;
+            }
+        }
+
+        StringBuilder sb = new StringBuilder();
+        sb.append(sx-5000).append(" ").append(sy-5000);
+        System.out.println(sb);
+
+
+    }
+
+}
