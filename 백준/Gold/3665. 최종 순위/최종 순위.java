03efbcec326e20340b3dff7ce2bbd5faaa8db5a4
Park Byeongchan <47100335+qudcks8084@users.noreply.github.com>
2025-03-26 11:42:24 +0900

    [Gold I] Title: 최종 순위, Time: 624 ms, Memory: 90956 KB -BaekjoonHub

--- 백준/Gold/3665. 최종 순위/최종 순위.java
+++ 백준/Gold/3665. 최종 순위/최종 순위.java
@@ -1,102 +1,100 @@
 import java.io.BufferedReader;
+import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.ArrayDeque;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Queue;
 import java.util.StringTokenizer;
 
 public class Main {
-    public static void main(String[] args) throws Exception {
+    static int T, N, C;
+    static int comein[];
+    static ArrayList<Integer> adjList[];
+    static Queue<Integer> q;
+    static int count;
+    
+    public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
+        StringBuilder sb;
 
-        int T = Integer.parseInt(br.readLine());
-
-        testcase : for(int testcase = 1 ; testcase <= T ; testcase++){
-
-            int N = Integer.parseInt(br.readLine());
-
-            // 관계 저장을 위한 adjMatrix 를 생성
-            boolean[][] adjMatrix = new boolean[N][N];
-
-            // 작년의 등수를 입력 받음
-            int[] prev_rank = new int[N];
-            StringTokenizer st = new StringTokenizer(br.readLine());
-            for(int i = 0 ;  i < N ; i++){
-                prev_rank[i] = Integer.parseInt(st.nextToken()) - 1;
+        T = Integer.parseInt(br.readLine());
+        for (int t = 0; t < T; t++) {
+            sb = new StringBuilder();
+            N = Integer.parseInt(br.readLine());
+            comein = new int[N + 1];
+            adjList = new ArrayList[N + 1];
+            q = new ArrayDeque<Integer>();
+            count = 0;
+            
+            for (int i = 1; i <= N; i++) {
+                adjList[i] = new ArrayList<Integer>();
             }
 
-            // 작년의 등수를 이용해서 이전 상관관계 집어넣기
-            for(int i = 0 ; i < N ; i++){
-                for (int j = i + 1; j < N; j++) {
-                    adjMatrix[prev_rank[i]][prev_rank[j]] = true;
+//            int team[] = Arrays.stream(br.readLine().split(" "))
+//                    .mapToInt(Integer::parseInt)
+//                    .toArray();
+            
+            int[] team = new int[N];
+            st = new StringTokenizer(br.readLine());
+            for(int i = 0 ; i < N ; i++) {
+            	team[i] = Integer.parseInt(st.nextToken());
+            }
+
+            comein[0] = -1;
+            for (int i = 1; i <= N; i++) {
+                for (int j = i + 1; j <= N; j++) {
+                    adjList[team[i - 1]].add(team[j - 1]);
+                    comein[team[j - 1]]++;
                 }
             }
 
-            // 올해 수정되는 등수를 입력받음
-            int numOfModify = Integer.parseInt(br.readLine());
-
-            boolean notAllowed = false;
-            for(int i = 0 ; i < numOfModify ; i++) {
+            int start = -1, end = -1;
+            C = Integer.parseInt(br.readLine());
+            for (int i = 0; i < C; i++) {
                 st = new StringTokenizer(br.readLine());
-                int prev = Integer.parseInt(st.nextToken()) - 1;
-                int next = Integer.parseInt(st.nextToken()) - 1;
-
-                // 데이터의 일관성을 퐉인 만약 순위가 바뀌지 않는 데이터가 들어온다면 "IMPOSSIBLE" 출력
-                // 이전 대회에서 거꾸로된 결과가 존재한다면
-                if (adjMatrix[next][prev]) {
-                    adjMatrix[next][prev] = false;
-                    adjMatrix[prev][next] = true;
-                }else if(adjMatrix[prev][next]){
-                    adjMatrix[prev][next] = false;
-                    adjMatrix[next][prev] = true;
-                }else{
-                    notAllowed = true;
+                start = Integer.parseInt(st.nextToken());
+                end = Integer.parseInt(st.nextToken());
+                if (adjList[start].contains(Integer.valueOf(end))) {
+                    adjList[start].remove(Integer.valueOf(end));
+                    comein[end]--;
+                    adjList[end].add(start);
+                    comein[start]++;
+                } else if (adjList[end].contains(Integer.valueOf(start))) {
+                    adjList[end].remove(Integer.valueOf(start));
+                    comein[start]--;
+                    adjList[start].add(end);
+                    comein[end]++;
                 }
             }
-
-            if(notAllowed){
-                System.out.println("IMPOSSIBLE");
-                continue testcase;
+            
+            for (int i = 1; i <= N; i++) {
+                if (comein[i] == 0) q.offer(i);
             }
-
-            // 전입 차수 Input_degree를 측정
-            int[] input_degree = new int[N];
-            for(int i = 0 ; i < N ; i++){
-                for(int j = 0 ; j < N ; j++){
-                    if(adjMatrix[j][i]) input_degree[i]++;
+            
+            int curV;
+            int next;
+            boolean ambiguous = false;
+            while (!q.isEmpty()) {
+                if (q.size() > 1) {
+                    ambiguous = true;
+                    break;
+                }
+                curV = q.poll();
+                sb.append(curV).append(" ");
+                count++;
+                for (int i = 0; i < adjList[curV].size(); i++) {
+                    next = adjList[curV].get(i);
+                    comein[next]--;
+                    if (comein[next] == 0) q.offer(next);
                 }
             }
-
-            // Topology Sort 를 위한 큐를 생성 및 전입 차수가 0인 것을 집어넣기
-            ArrayDeque<Integer> q = new ArrayDeque<>();
-            for(int i = 0 ; i < N ; i++){
-                if(input_degree[i] == 0) q.offer(i);
-            }
-
-            int numOfRank = 0;
-            StringBuilder sb = new StringBuilder();
-            while (!q.isEmpty()){
-                numOfRank++;
-                int cur = q.poll();
-                sb.append(cur + 1).append(" ");
-
-                for(int i = 0 ; i < N ; i++){
-                    if(adjMatrix[cur][i]){ // 현재 위치에서 연결되어있다면
-                        input_degree[i]--;
-                        if(input_degree[i] == 0){
-                            q.offer(i);
-                        }
-                    }
-                }
-
-            }
-
-            if(numOfRank == N){
-                System.out.println(sb);
-            }else{
-                System.out.println("IMPOSSIBLE");
-            }
-
-        }
-
+            if (count != N || ambiguous) {
+                sb = new StringBuilder();
+                sb.append("IMPOSSIBLE").append("\n");
+            } else sb.append("\n");
+            System.out.print(sb);
+        }    
     }
-
 }
