bc57aac65896c761f07f32732f5cde041693758d
Park Byeongchan <47100335+qudcks8084@users.noreply.github.com>
2025-04-03 09:47:09 +0900

    [Silver I] Title: 신입 사원, Time: 2772 ms, Memory: 313744 KB -BaekjoonHub

--- 백준/Silver/1946. 신입 사원/신입 사원.java
+++ 백준/Silver/1946. 신입 사원/신입 사원.java
@@ -2,15 +2,13 @@ import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.util.Arrays;
 import java.util.StringTokenizer;
-import java.util.TreeSet;
 
 public class Main {
 
     static class people {
-        int index, document, meeting;
+        int document, meeting;
 
-        public people(int index, int document, int meeting) {
-            this.index = index;
+        public people(int document, int meeting) {
             this.document = document;
             this.meeting = meeting;
         }
@@ -30,25 +28,23 @@ public class Main {
                 StringTokenizer st = new StringTokenizer(br.readLine());
                 int d = Integer.parseInt(st.nextToken());
                 int m = Integer.parseInt(st.nextToken());
-                arr[i] = new people(i, d, m);
+                arr[i] = new people(d, m);
             }
-
-            TreeSet<Integer> result = new TreeSet<>();
 
             // 먼저 서류가 높은거부터 검토
             Arrays.sort(arr, ((o1, o2) -> Integer.compare(o1.document, o2.document)));
 
             // 위에서부터 검토, 단. 1번째꺼는 무조건 통과
-            result.add(arr[0].index);
+            int cnt = 1;
             int max_meeting = arr[0].meeting;
             for (int i = 1 ; i < N ; i++) {
                 if(max_meeting > arr[i].meeting){
-                    result.add(arr[i].index);
+                    cnt++;
                     max_meeting = arr[i].meeting;
                 }
             }
 
-            sb.append(result.size()).append("\n");
+            sb.append(cnt).append("\n");
         }
 
         System.out.println(sb);
