7f362982399f96d8cf32a26dddc3c456abf7fa74
Park Byeongchan <47100335+qudcks8084@users.noreply.github.com>
2025-04-03 10:29:05 +0900

    [Silver I] Title: 신입 사원, Time: 744 ms, Memory: 297680 KB -BaekjoonHub

--- 백준/Silver/1946. 신입 사원/신입 사원.java
+++ 백준/Silver/1946. 신입 사원/신입 사원.java
@@ -5,15 +5,6 @@ import java.util.StringTokenizer;
 
 public class Main {
 
-    static class people {
-        int document, meeting;
-
-        public people(int document, int meeting) {
-            this.document = document;
-            this.meeting = meeting;
-        }
-    }
-
     public static void main(String[] args) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringBuilder sb = new StringBuilder();
@@ -23,24 +14,21 @@ public class Main {
         for(int t = 0 ; t < T ; t++){
             int N = Integer.parseInt(br.readLine());
 
-            people[] arr = new people[N];
+            int[] arr = new int[N];
             for(int i = 0 ; i < N ; i++){
                 StringTokenizer st = new StringTokenizer(br.readLine());
-                int d = Integer.parseInt(st.nextToken());
+                int d = Integer.parseInt(st.nextToken()) - 1;
                 int m = Integer.parseInt(st.nextToken());
-                arr[i] = new people(d, m);
+                arr[d] = m;
             }
 
-            // 먼저 서류가 높은거부터 검토
-            Arrays.sort(arr, ((o1, o2) -> Integer.compare(o1.document, o2.document)));
-
             // 위에서부터 검토, 단. 1번째꺼는 무조건 통과
-            int cnt = 1;
-            int max_meeting = arr[0].meeting;
-            for (int i = 1 ; i < N ; i++) {
-                if(max_meeting > arr[i].meeting){
+            int cnt = 1; // 첫 번째 후보자는 항상 통과
+            int max_meeting = arr[0];
+            for (int i = 1; i < N; i++) {
+                if (max_meeting > arr[i]) {
                     cnt++;
-                    max_meeting = arr[i].meeting;
+                    max_meeting = arr[i];
                 }
             }
 
