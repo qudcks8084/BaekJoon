1b49c43e97d52afa0bfbb928d03d8cda4913431d
Park Byeongchan <47100335+qudcks8084@users.noreply.github.com>
2025-04-03 18:42:45 +0900

    [Gold V] Title: 주사위 쌓기, Time: 164 ms, Memory: 19588 KB -BaekjoonHub

--- 백준/Gold/2116. 주사위 쌓기/주사위 쌓기.java
+++ 백준/Gold/2116. 주사위 쌓기/주사위 쌓기.java
@@ -1,38 +1,28 @@
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.util.StringTokenizer;
-
 public class Main {
-
     static int N;
     static int[][] dice;
     static int[] opposite = {5, 3, 4, 1, 2, 0}; // 주사위 각 면의 반대편 인덱스
-
     public static void main(String[] args) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-
         N = Integer.parseInt(br.readLine());
         dice = new int[N][6];
-
         for(int i = 0; i < N; i++){
             StringTokenizer st = new StringTokenizer(br.readLine());
             for(int j = 0; j < 6; j++){
                 dice[i][j] = Integer.parseInt(st.nextToken());
             }
         }
-
         int answer = 0;
-
         // 첫 번째 주사위의 윗면을 선택
         for(int firstTop = 0; firstTop < 6; firstTop++) {
             int firstBottom = opposite[firstTop];
-
             // 최댓값 찾기
             int sum = getMaxSide(0, firstTop, firstBottom);
-
             // 현재 윗면 값 (다음 주사위의 아랫면과 일치해야 함)
             int currentTop = dice[0][firstTop];
-
             // 두 번째 주사위부터 쌓기
             for(int i = 1; i < N; i++) {
                 int bottomIdx = findFaceIndex(i, currentTop);
@@ -40,22 +30,21 @@ public class Main {
                 sum += getMaxSide(i, topIdx, bottomIdx);
                 currentTop = dice[i][topIdx];
             }
-
             // 최댓값 갱신
             answer = Math.max(answer, sum);
         }
-
         System.out.println(answer);
     }
 
     public static int getMaxSide(int diceIdx, int topIdx, int bottomIdx) {
-        // 주사위 값은 1~6이므로 큰 값부터 확인
+        int topValue = dice[diceIdx][topIdx];
+        int bottomValue = dice[diceIdx][bottomIdx];
+
+        // 6부터 1까지 내림차순으로 검사
         for(int value = 6; value >= 1; value--) {
-            for(int i = 0; i < 6; i++) {
-                // 해당 값이 주사위에 있고, 윗면이나 아랫면이 아니라면 즉시 반환
-                if(dice[diceIdx][i] == value && i != topIdx && i != bottomIdx) {
-                    return value;
-                }
+            // 현재 value가 top이나 bottom의 값이 아니면 즉시 반환
+            if(value != topValue && value != bottomValue) {
+                return value;
             }
         }
         return 0;
