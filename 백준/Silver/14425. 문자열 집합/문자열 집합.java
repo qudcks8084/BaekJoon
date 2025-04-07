5f1e2dad3db68e6322e4a5357b393ec9a5d9ffbe
Park Byeongchan <47100335+qudcks8084@users.noreply.github.com>
2025-04-07 09:26:16 +0900

    [Silver IV] Title: 문자열 집합, Time: 292 ms, Memory: 38960 KB -BaekjoonHub

--- 백준/Silver/14425. 문자열 집합/문자열 집합.java
+++ 백준/Silver/14425. 문자열 집합/문자열 집합.java
@@ -1,60 +1,9 @@
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
+import java.util.HashSet;
 import java.util.StringTokenizer;
 
 public class Main {
-	static class TrieNode {
-		boolean isLeafNode;
-		TrieNode[] children;
-
-		public TrieNode() {
-			this.isLeafNode = false;
-			this.children = new TrieNode[26];
-		}
-	}
-	
-	static class Trie{
-		TrieNode root;
-
-		public Trie() {
-			root = new TrieNode();
-		}
-		
-		public void insert(String word) {
-			TrieNode cur = root;
-			
-			for(char c : word.toCharArray()) {
-				int index = c - 'a';
-				
-				if(cur.children[index] == null) {
-					cur.children[index] = new TrieNode();
-				}
-				
-				cur = cur.children[index];
-			}
-			
-			cur.isLeafNode = true;
-		}
-		
-		public boolean hasWord(String word) {
-			TrieNode cur = root;
-			
-			for(char c : word.toCharArray()) {
-				int index = c - 'a';
-				
-				if(cur.children[index] == null) {
-					return false;
-				}
-				
-				cur = cur.children[index];
-			}
-			
-			if(cur.isLeafNode) return true;
-			
-			return false;
-		}
-		
-	}
 	
 	public static void main(String[] args) throws Exception {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
@@ -63,14 +12,14 @@ public class Main {
 		int N = Integer.parseInt(st.nextToken());
 		int M = Integer.parseInt(st.nextToken());
 		
-		Trie trie = new Trie();
+		HashSet<String> set = new HashSet<>();
 		for(int i = 0 ; i < N ; i++) {
-			trie.insert(br.readLine());
+			set.add(br.readLine());
 		}
 		
 		int cnt = 0;
 		for(int i = 0 ; i < M ; i++) {
-			if(trie.hasWord(br.readLine())) cnt++;
+			if(set.contains(br.readLine())) cnt++;
 		}
 		
 		System.out.println(cnt);
