8a575236cf055c5835ef3a302f5d08edd5889d9c
SIDEDISH <47100335+qudcks8084@users.noreply.github.com>
2025-05-18 19:24:57 +0900

    [Bronze V] Title: 웰컴, Time: 60 ms, Memory: 11224 KB -BaekjoonHub

---
+++ 백준/Bronze/5337. 웰컴/웰컴.java
@@ -0,0 +1,8 @@
+
+public class Main {
+    public static void main(String[] args) {
+        StringBuilder sb = new StringBuilder();
+        sb.append(".  .   .\n").append("|  | _ | _. _ ._ _  _\n").append("|/\\|(/.|(_.(_)[ | )(/.");
+        System.out.println(sb);
+    }
+}
