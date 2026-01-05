8b44a30614412e52e195ed48b8f2d1fdee58fd62
SIDEDISH <47100335+qudcks8084@users.noreply.github.com>
2026-01-05 10:13:20 +0900

    [Gold V] Title: 토마토, Time: 448 ms, Memory: 101284 KB -BaekjoonHub

--- 백준/Gold/7576. 토마토/토마토.java
+++ 백준/Gold/7576. 토마토/토마토.java
@@ -1,64 +1,64 @@
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
-import java.util.ArrayDeque;
-import java.util.ArrayList;
-import java.util.StringTokenizer;
-
+import java.util.*;
+import java.io.*;
 public class Main {
 
-    public static void main(String[] args) throws Exception {
+    static int H, W;
+    static int[][] farm;
 
+    static int[] dc = {0, 1, 0, -1};
+    static int[] dr = {1, 0, -1, 0};
+    public static void main(String[] args) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
         StringTokenizer st = new StringTokenizer(br.readLine());
-        int W = Integer.parseInt(st.nextToken());
-        int H = Integer.parseInt(st.nextToken());
 
-        int[][] map = new int[H][W];
-        
-        int numOfNotGoodTomato = 0; // 정답 확인용 덜익은 토마토의 개수 저장
+        W = Integer.parseInt(st.nextToken());
+        H = Integer.parseInt(st.nextToken());
+
+        farm = new int[H][W];
+
+        // 시작 토마토를 담아둘 큐를 초기화
         ArrayDeque<int[]> q = new ArrayDeque<>();
-        
-        for(int c = 0 ; c < H ; c++) {
+
+        int numOfNotGoodTomato = 0;
+        for(int c = 0 ; c < H ; c++){
             st = new StringTokenizer(br.readLine());
-            for(int r = 0 ; r < W ; r++) {
-                map[c][r] = Integer.parseInt(st.nextToken());
-                if(map[c][r] == 1) q.offer(new int[] {c,r});
-                if(map[c][r] == 0) numOfNotGoodTomato++;
+            for(int r = 0 ; r < W ; r++){
+                int tmp = Integer.parseInt(st.nextToken());
+                farm[c][r] = tmp;
+                if(tmp == 1){
+                    q.offer(new int[]{c, r});
+                }else if(tmp == 0) {
+                    numOfNotGoodTomato++;
+                }
             }
         }
 
-
-        int[] dc = {-1, 0, 1 ,0};
-        int[] dr = {0, 1, 0, -1};
         int time = 0;
-        while(true) {
+        while(!q.isEmpty()){
+            if(numOfNotGoodTomato == 0) break;
             int len = q.size();
-            for(int i = 0 ; i < len ; i++) {
+            for(int k = 0 ; k < len ; k++){
                 int[] cur = q.poll();
-                for(int j = 0 ; j < 4 ; j++) {
-                    int n_c = cur[0] + dc[j];
-                    int n_r = cur[1] + dr[j];
-                    if(n_c < 0 || n_c >= H || n_r < 0 || n_r >= W) continue;
-                    if(map[n_c][n_r] == 0) {
+                for(int i = 0 ; i < 4 ; i++){
+                    int nc = cur[0] + dc[i];
+                    int nr = cur[1] + dr[i];
+
+                    if(nc < 0 || nc >= H || nr < 0 || nr >= W) continue;
+                    if(farm[nc][nr] == 0){
                         numOfNotGoodTomato--;
-                        map[n_c][n_r] = 1;
-                        q.offer(new int[] {n_c, n_r});
+                        farm[nc][nr] = 1;
+                        q.offer(new int[]{nc, nr});
                     }
                 }
-            }
-            if(q.isEmpty()) {
-                break;
             }
             time++;
         }
 
-        if(numOfNotGoodTomato == 0) {
+        if(numOfNotGoodTomato == 0){
             System.out.println(time);
-        }else {
-            System.out.println("-1");
+        }else{
+            System.out.println(-1);
         }
-
     }
-
 }
