7f70882ba18eee192e91ee60e7a7c42a90678a40
SIDEDISH <47100335+qudcks8084@users.noreply.github.com>
2026-01-06 20:13:50 +0900

    [level 0] Title: 피자 나눠 먹기 (2), Time: 0.03 ms, Memory: 90.8 MB -BaekjoonHub

---
+++ 프로그래머스/0/120815. 피자 나눠 먹기 （2）/피자 나눠 먹기 （2）.java
@@ -0,0 +1,8 @@
+class Solution {
+    public int solution(int n) {
+        if(n % 6 == 0) return n / 6;
+        else if(n % 3 == 0) return n / 3;
+        else if(n % 2 == 0) return n / 2;
+        else return n;
+    }
+}
