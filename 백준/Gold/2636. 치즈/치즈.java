1291ca4bed24cab81b20132932e2ef131ccbe082
SIDEDISH <47100335+qudcks8084@users.noreply.github.com>
2026-01-08 16:02:19 +0900

    [Gold IV] Title: 치즈, Time: 76 ms, Memory: 12180 KB -BaekjoonHub

--- 백준/Gold/2636. 치즈/치즈.java
+++ 백준/Gold/2636. 치즈/치즈.java
@@ -1,81 +1,106 @@
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
-import java.util.ArrayDeque;
-import java.util.StringTokenizer;
+import java.util.*;
+import java.io.*;
 
 public class Main {
-	
-	static public class Block {
-		int c, r;
-		public Block(int c, int r) {
-			this.c = c;
-			this.r = r;
-		}
-	}
-	
-	static int H, W;
-	static boolean[][] map, visited;
-	static int[] dc = {-1,0,1,0};
-	static int[] dr = {0,1,0,-1};
+    static int H, W;
+    static boolean[][] map;
+    static boolean[][] visited;
 
-	public static void main(String[] args) throws Exception {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		
-		StringTokenizer st = new StringTokenizer(br.readLine());
-		H = Integer.parseInt(st.nextToken());
-		W = Integer.parseInt(st.nextToken());
-		map = new boolean[H][W];
-		int num_of_cheese = 0;
-		
-		// 입력
-		for(int i = 0 ; i < H ;  i++) {
-			st = new StringTokenizer(br.readLine());
-			for(int j = 0 ; j < W ; j++) {
-				map[i][j] = st.nextToken().equals("1");
-				if(map[i][j]) num_of_cheese++;
-			}
-		}
-		
-		int time = 1;
-		while(true) {
-			int num_of_melt_cheese = removeCheese();
-			if(num_of_melt_cheese == num_of_cheese) {
-				StringBuilder sb = new StringBuilder();
-				sb.append(time).append("\n").append(num_of_cheese);
-				System.out.println(sb);
-				break;
-			}
-			num_of_cheese -= num_of_melt_cheese;
-			time++;
-		} 
-	
-	}
-	
-	// 가장 외부에 있는 치즈를 삭제
-	public static int removeCheese() { 
-		int melting = 0;
-		visited = new boolean[H][W];
-		ArrayDeque<Block> q = new ArrayDeque<>();
-		q.offer(new Block(0, 0)); // 가장 외부 공기에서부터 접근
-		visited[0][0] = true;
-		while(!q.isEmpty()) {
-			Block now = q.poll();
-			for(int i = 0 ; i < 4 ; i++) {
-				int n_c = now.c + dc[i];
-				int n_r = now.r + dr[i];
-				
-				if(n_c < 0 || n_c >= H || n_r < 0 || n_r >= W || visited[n_c][n_r]) continue;
-				
-				visited[n_c][n_r] = true;
-				
-				if(map[n_c][n_r]) {
-					map[n_c][n_r] = false;
-					melting++;
-				} 
-				else q.offer(new Block(n_c, n_r));
-				
-			}
-		}
-		return melting;
-	}
+    static int[] dc = {-1, 0, 1, 0};
+    static int[] dr = {0, 1, 0, -1};
+    public static void main(String[] args) throws Exception {
+
+        BufferedReader br =  new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        H = Integer.parseInt(st.nextToken());
+        W = Integer.parseInt(st.nextToken());
+
+        map = new boolean[H][W];
+
+        int numOfCheese = 0 ;
+
+        for(int c = 0 ; c < H ; c++){
+            st = new StringTokenizer(br.readLine());
+            for(int r = 0 ; r < W ; r++){
+                boolean isCheese = st.nextToken().equals("1");
+                if(isCheese) numOfCheese++;
+                map[c][r] = isCheese;
+            }
+        }
+
+        int time = 0;
+        ArrayDeque<int[]> q = new ArrayDeque<>();
+        ArrayDeque<int[]> melt = new ArrayDeque<>();
+
+        visited = new boolean[H][W];
+
+//        print();
+
+        q.offer(new int[]{0, 0});
+        visited[0][0] = true;
+
+        while(true){
+
+            while(!q.isEmpty()){
+                int[] cur = q.poll();
+
+                for(int i = 0 ; i < 4 ; i++){
+                    int nc = cur[0] + dc[i];
+                    int nr = cur[1] + dr[i];
+
+                    if(nc < 0 || nc >= H || nr < 0 || nr >= W) continue;
+                    if(visited[nc][nr]) continue;
+
+                    visited[nc][nr] = true;
+
+                    // 만약 치즈인 경우
+                    if(map[nc][nr]){
+                        melt.offer(new int[]{nc, nr});
+                    }else{
+                        q.offer(new int[]{nc, nr});
+                    }
+                }
+            }
+
+            // 만약 이번턴에 전부 종료되는 경우
+            if(numOfCheese == melt.size()){
+                StringBuilder sb = new StringBuilder();
+                sb.append(time + 1).append("\n").append(numOfCheese);
+                System.out.println(sb);
+                break;
+            }
+
+            // 아닌 경우
+            numOfCheese -= melt.size();
+            while(!melt.isEmpty()){
+                int[] cur = melt.poll();
+                map[cur[0]][cur[1]] = false;
+                q.offer(cur);
+            }
+
+//            print();
+
+            time++;
+
+        }
+    }
+
+    public static void print(){
+        StringBuilder sb = new StringBuilder();
+        sb.append("\n");
+        for(int c = 0 ; c < H ; c++){
+            for(int r = 0 ; r < W ; r++){
+                sb.append(map[c][r] ? "x" : "-").append(" ");
+            }
+
+            sb.append("\t");
+
+            for(int r = 0 ; r < W ; r++){
+                sb.append(visited[c][r] ? "x" : "-").append(" ");
+            }
+            sb.append("\n");
+        }
+        System.out.println(sb);
+    }
 }
